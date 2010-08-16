26e9b39776bead67410384b9cd1c15045d3230a7
Timo Paulssen <timonator@perpetuum-immobile.de>
2010-08-16 03:51:39 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
         name = "lojbantools",
-        version = "0.2.1",
+        version = "0.2.2",
 
         # metadata for PYPI
         author = "Timo Paulssen",
         author_email = "timo+lojbantools@wakelift.de",
         description = "This package offers access to a bunch of lojban tools.",
         license = "BSD",
-        download_url = "http://wakelift.de/lojban/software/python/lojbantools-0.2.1.tar.gz",
+        download_url = "http://wakelift.de/lojban/software/python/lojbantools-0.2.2.tar.gz",
 
         py_modules = ["camxes"],
 
