8d411ed68c59c77b4f0aab9315b315d81a8ef193
Timo Paulssen <timonator@perpetuum-immobile.de>
2010-08-16 03:50:25 +0200

    a rudimentary test suite is better than none.

---
+++ tests/test_camxes.py
@@ -0,0 +1,27 @@
+from camxes import call_camxes, call_vlatai, selmaho
+from textwrap import dedent
+
+def camxes_assert_equal(text, arguments, result):
+    res = call_camxes(text, arguments) 
+    assert res == result,\
+        dedent("""called camxes with %r, text %r.
+        expected result: %r
+        got instead:     %r""") % (arguments, text, result, res)
+
+def test_camxes_text():
+    camxes_assert_equal("cipra ui sai", "-t", "cipra ui sai")
+    camxes_assert_equal("oi fliba cu fliba", "-t", "oi fliba")
+    camxes_assert_equal("hooah!", "-t", "")
+
+def test_camxes_tree():
+    assert "cipra" in call_camxes("cipra")
+    assert "gerku" in call_camxes("lo gerku cu klama")
+
+def test_vlatai():
+    assert call_vlatai("jbopre") == ["jbopre", "lujvo", "jbopre"]
+    assert call_vlatai("gerku") == ["gerku", "gismu", "gerku"]
+    assert call_vlatai("hello") == ["hello", "UNMATCHED", "hello"]
+
+def test_makfa():
+    assert selmaho("u'i")[1] == ["UI1"]
+    assert selmaho("coi")[1] == ['COI', 'DOhU']
