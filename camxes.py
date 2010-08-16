138d54fca4ec3ae52ba82ecf223bb837b6966a41
Timo Paulssen <timonator@perpetuum-immobile.de>
2010-08-16 03:50:14 +0200

    refactored downloading, fixed call_camxes.

--- camxes.py
+++ camxes.py
@@ -18,26 +18,70 @@ class CouldNotFindCamxesError(Exception):
     pass
 
 
+def download_camxes(url=None):
+    if not url:
+        url = __CAMXES_URL__
+
+    try:
+        import md5
+        import urllib2
+
+        download = urllib2.urlopen(__CAMXES_URL__)
+        digest = md5.new()
+
+        camxesdata = download.read()
+        digest.update(camxesdata)
+
+        if (digest.hexdigest() != __CAMXES_MD5SUM__ and
+               __CAMXES_MD5SUM__):
+            print ("downloaded camxes from the website, "
+                  "but the md5 did not match.")
+            raise
+
+        lojbanfolder = os.path.expanduser("~/lojban")
+        if not os.path.exists(lojbanfolder):
+            os.mkdir(lojbanfolder)
+
+        camxespath = os.path.join(lojbanfolder,
+                                  "lojban_peg_parser.jar")
+        open(camxespath, "w").write(camxesdata)
+        return camxespath
+    except:
+        pass
+    
+    return None
+
+
+def locate_camxes():
+    locate = subprocess.Popen(["locate", "lojban_peg_parser.jar"],
+                              stdout=subprocess.PIPE)
+    
+    valids = []
+    
+    for found in locate.stdout.readlines():
+        try:
+            open(found.strip(), "r").close()
+        except IOError:
+            continue
+        valids.append(found.strip())
+
+    if not valids:
+        raise CouldNotFindCamxesError()
+
+    camxespath = valids[0]
+    return camxespath
+
+
 def find_camxes():
     global camxespath
     if camxespath:
         return camxespath
-    valids = []
+    
     try:
-        locate = subprocess.Popen(["locate", "lojban_peg_parser.jar"],
-                                  stdout=subprocess.PIPE)
-        for found in locate.stdout.readlines():
-            try:
-                open(found.strip(), "r").close()
-            except IOError:
-                continue
-            valids.append(found.strip())
+        camxespath = locate_camxes()
+        if camxespath:
+            return camxespath
 
-        if not valids:
-            raise CouldNotFindCamxesError()
-
-        camxespath = valids[0]
-        return camxespath
     except (OSError, CouldNotFindCamxesError):
         try:
             # see if the file has already been downloaded.
@@ -48,33 +92,9 @@ def find_camxes():
             return camxespath
 
         except IOError:
-            try:
-                # try to download the file
-                import md5
-                import urllib2
-
-                download = urllib2.urlopen(__CAMXES_URL__)
-                digest = md5.new()
-
-                camxesdata = download.read()
-                digest.update(camxesdata)
-
-                if (digest.hexdigest() != __CAMXES_MD5SUM__ and
-                       __CAMXES_MD5SUM__):
-                    print ("downloaded camxes from the website, "
-                          "but the md5 did not match.")
-                    raise
-
-                lojbanfolder = os.path.expanduser("~/lojban")
-                if not os.path.exists(lojbanfolder):
-                    os.mkdir(lojbanfolder)
-
-                camxespath = os.path.join(lojbanfolder,
-                                          "lojban_peg_parser.jar")
-                open(camxespath, "w").write(camxesdata)
+            camxespath = download_camxes()
+            if camxespath:
                 return camxespath
-            except:
-                pass
 
     print "could not find camxes by using locate, looking into",
     print os.path.expanduser("~/lojban/lojban_peg_parser.jar"),
@@ -91,24 +111,36 @@ def find_vlatai():
 camxesinstances = {}
 
 
-def call_camxes(text, arguments=()):
+def call_camxes(text, *arguments):
     global camxesinstances
-    arguments = tuple(arguments)
+    if not isinstance(arguments, tuple):
+        arguments = tuple(arguments)
+
     if arguments in camxesinstances:
-        sp = camxesinstances[arguments]
+        instance = camxesinstances[arguments]
+
     else:
         camxesPath = find_camxes()
-        sp = subprocess.Popen(["java", "-jar", camxesPath] + list(arguments),
+        instance = subprocess.Popen(["java", "-jar", camxesPath] + list(arguments),
                               stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         # eat the "hello" line for each of the arguments
         for _ in arguments:
-            a = sp.stdout.readline()
-        camxesinstances[arguments] = sp
-    sp.stdin.write(text)
-    sp.stdin.write("\n")
-    a = sp.stdout.readline()
-    #newline = sp.stdout.readline()
-    return a
+            a = instance.stdout.readline()
+        camxesinstances[arguments] = instance
+
+    instance.stdin.write(text + "\n")
+
+    result = ""
+
+    # if camxes is started with -t, it will not print two newlines at the end
+    if "-t" in arguments:
+        delim = "\n"
+    else:
+        delim = "\n\n"
+    while not result.endswith(delim):
+        result += instance.stdout.read(1)
+
+    return result.strip()
 
 
 def call_vlatai(text):
